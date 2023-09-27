f5fd115c2f60d8552106a1117d43dada154c1655
Scott <scottmay612@gmail.com>
2023-09-27 16:09:25 -0600

    I created the program.cs file and resume.cs file.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startyear = 2020;
+        job1._endyear = 2023;
+
+        Job job2 = new();
+        job2._jobTitle = "Computer Engineer";
+        job2._company = "Apple";
+        job2._startyear = 2013;
+        job2._endyear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._personName = "Scott May";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
